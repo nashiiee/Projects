2c3b1f55fe5261fc5c8fdd83cf67d9cbee73d6e5
Nash <nashclaracay28@gmail.com>
2026-07-09 17:47:57 +0800

    Js Clock with setDate Function

---
+++ jsproj/JsClock/script.js
@@ -0,0 +1,19 @@
+const secondHand = document.querySelector('.second-hand');
+const hourHand = document.querySelector('.hour-hand');
+const minutesHand = document.querySelector('.min-hand');
+
+function setDate() {
+  const now = new Date();
+  const seconds = now.getSeconds();
+  const hours = now.getHours();
+  const minutes = now.getMinutes();
+  const hoursDegrees = ((hours / 12) * 360) + 90;
+  const minutesDegrees = ((minutes / 60) * 360) + 90;
+  const secondsDegrees = ((seconds / 60) * 360) + 90;
+
+  hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+  minutesHand.style.transform = `rotate(${minutesDegrees}deg)`;
+}
+
+setInterval(setDate, 1000);
